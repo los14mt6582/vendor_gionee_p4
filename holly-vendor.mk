f99b13031c1e3f00d1270c51af7466ae7b925a46
Kishan <kishannn.p.19@gmail.com>
2017-09-06 18:08:04 +0530

    Holly : Vendor files

---
+++ holly-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/holly/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/holly/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/holly/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/holly/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/holly/proprietary/xbin,system/xbin)
+
